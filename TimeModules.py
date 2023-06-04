5c4a71b1daade54e921943d82ca454681bd33baf
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-04 19:58:28 +0530

    Update TimeModules.py

--- TimeModules.py
+++ TimeModules.py
@@ -1,19 +1,19 @@
 import time
-# initial= time.time()
-# print(initial)
-# k=0
-# while(k<86):
-#     print("This is Harsh")
-#     k+=1
-# print("While loop ran in",time.time()-initial,"Seconds")
+initial= time.time()
+print(initial)
+k=0
+while(k<86):
+    print("This is Harsh")
+    k+=1
+print("While loop ran in",time.time()-initial,"Seconds")
 localtime=time.asctime(time.localtime(time.time()))
 print(localtime)
 
-# k=0
-# while(k<10):
-#     print("This is Harsh")
-#     time.sleep(3)
-#     k+=1
+k=0
+while(k<10):
+    print("This is Harsh")
+    time.sleep(3)
+    k+=1
 localtime=time.asctime()
 print(localtime)
 
