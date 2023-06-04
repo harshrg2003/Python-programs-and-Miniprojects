259f09632fb5c09dc36e1fa9a7c0e31903b456ea
1RN21CS062 <117375175+harshrg2003@users.noreply.github.com>
2023-06-04 20:00:17 +0530

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,7 +1,7 @@
-#var1="Hello world"
-#print(var1)
-#var6=" welcome to python programming"
-#print(var1 + var6)
+var1="Hello world"
+print(var1)
+var6=" welcome to python programming"
+print(var1 + var6)
 """
 var2=4
 var3=36.7
@@ -14,9 +14,8 @@ var1="54"
 var2="32"
 print(int(var1)+int(var2))
 """
-#print("Enter ur number")
-#inpnum=input()
-#print("You entered",int(inpnum)+20)
-#print(10*"Hello world\t")
+print("Enter ur number")#inpnum=input()
+print("You entered",int(inpnum)+20)
+print(10*"Hello world\t")
 mystr="Harsh is a good boy"
 print(mystr[0:8])
